9d44f666b7a97c1162d5bf44fa8084e10d44f614
idiot65 <oldrandomdude@gmail.com>
2026-04-10 19:33:19 -0400

    created three pages with working buttons (org, employee, backend)

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/org")
+def org():
+    return render_template("org.html")
+
+@app.route("/employee")
+def employee():
+    return render_template("employee.html")
+
+@app.route("/backend")
+def backend():
+    return render_template("backend.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
--- readme.md
+++ readme.md
@@ -3,11 +3,3 @@ x = 4y
 f = diff(x)
 
 Jai
-
-
-Colors:
-Black - #000000
-Light Blue - #ADD8E6
-Cream - #FFFFE0
-Light Green - #B6DB98
-Peach - #FF997A
