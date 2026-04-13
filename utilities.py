6dae02dedf2225aa432199fdd6b3d98b42d4e4d5
ilisien-academic <irl5102@psu.edu>
2026-04-13 20:20:14 +0000

    Apr 13, 2026, 4:20 PM

--- utilities.py
+++ utilities.py
@@ -1,6 +1,10 @@
 from faker import Faker 
 
 
+class Employee:
+    def __init__(name)
+
 def generate_employees(n_of_employees):
     fake = Faker()
-    for i_e in range(0,)
+    for i_e in range(n_of_employees):
+        
